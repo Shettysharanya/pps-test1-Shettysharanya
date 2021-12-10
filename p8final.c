0104fce437aee2d3bf8b76490fc9f144191df2d2
sharanya0709 <94777111+Shettysharanya@users.noreply.github.com>
2021-12-10 11:37:21 +0000

    p8original

--- p8final.c
+++ p8final.c
@@ -1,19 +1,28 @@
-#include <stdio.h>  
-#include<string.h>  
-int main()  
-{  
-   char str1[20];  // declaration of char array  
-   char str2[20];  // declaration of char array  
-   int value; // declaration of integer variable  
-   printf("Enter the first string : ");  
-   scanf("%s",str1);  
-   printf("Enter the second string : ");  
-   scanf("%s",str2);  
-   // comparing both the strings using strcmp() function  
-   value=strcmp(str1,str2);  
-   if(value==0)  
-   printf("strings are same");  
-   else  
-   printf("strings are not same");  
-   return 0;  
-}  
+#include<stdio.h>
+#include<math.h>
+
+int main() {
+	
+	int n;
+	double start,end,mid;
+	
+	printf("Enter a Number: ");
+	scanf("%d",&n);
+	
+	start = 0, end = n;
+	
+	while((end - start)>=0.000001){
+		
+		mid = (start + end)/2;
+		
+		if(mid*mid < n)
+			start = mid;
+		if(mid*mid >= n)
+			end = mid;
+		
+	}
+	
+	printf("Square Root of %d is %f",n,mid);
+	
+}
+Output
