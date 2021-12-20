bd921bd64b26798abc8aa53990f44d47b1819f31
sharanya0709 <94777111+Shettysharanya@users.noreply.github.com>
2021-12-20 06:19:59 +0000

    p3original

--- p5original.c
+++ p5original.c
@@ -1,21 +1,14 @@
 #include<stdio.h>
-
-// function to find largest among three number
-float large(float a, float b, float c)
+void input(int *a, int *b, int *c)
 {
-   if(a>=b && a>=c) return a;
-   else if(b>=a && b>=c) return b;
-   else return c;
+  printf("Enter the first number: ");
+  scanf("%d", a);
+  printf("enter the second number: ");
+  scanf("%d", b);
+  printf("Enter the third number:");
+  scanf("%d", c);
 }
-
-int main()
+void cmp(int a, int b, int *large)
 {
-   float num1, num2, num3, largest;
+  if(a>b)
 
-   printf("Enter three numbers: ");
-   scanf("%f %f %f", &num1, &num2, &num3);
-
-   largest = large(num1, num2, num3);
-   printf("Largest number = %.2f",largest);
-   return 0;
-}
